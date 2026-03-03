de669d52c5a0bcd45b236e623bf6468deca753cd
lukasksola <24ia17_rais@spsjc.cz>
2026-03-03 08:59:21 +0100

    Přidejte soubory projektu.

---
+++ drawing/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace drawing
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ShapeDrawer shapeDrawer;
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+            shapeDrawer = new ShapeDrawer();
+            shapeDrawer.mw = this;
+        }
+        public SolidColorBrush currentColor;
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            currentColor = new SolidColorBrush(Color.FromArgb(255, (byte)Slider1.Value, (byte)Slider2.Value, (byte)Slider3.Value));
+            ColorShower.Fill = currentColor;
+        }
+        public string CurrentShape;
+        private void NewShapeChosen(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button btn) {
+                CurrentShape = (string)btn.Content;
+            }
+        }
+        public Point MousePosition;
+
+        public void AddShapeToCanvas()
+        {
+
+        }
+
+        private void MainCanvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            MousePosition = e.MouseDevice.GetPosition(MainCanvas);
+            CursorPos.Text = $"{MousePosition.X} {MousePosition.Y}" ;
+        }
+
+        private void MainCanvas_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ drawing/ShapeDrawer.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace drawing
+{
+    public class ShapeDrawer
+    {
+        public sMainWindow mw;
+        public ShapeDrawer() { }
+
+        public bool WaitingForAnotherClick;
+        public List<Point> positions = new List<Point>(); 
+        public void DrawShape(string shape, SolidColorBrush brush, Point position)
+        {
+            if(shape == "Rectangle")
+            {
+                if(positions.Count == 0)
+                {
+                    positions.Add(position);
+                } else
+                {
+                    positions.Add(position);
+
+                    CreateRectagle(brush);
+
+                    positions.Clear();
+                }
+            } else if(shape == "Circle")
+            {
+
+            } else if(shape == "Line")
+            {
+
+            }
+
+        }
+
+        public void CreateRectagle(SolidColorBrush brush)
+        {
+            System.Windows.Shapes.Rectangle rect = new System.Windows.Shapes.Rectangle();
+            rect.Width = Math.Abs(positions[0].X - positions[1].X);
+            rect.Height = Math.Abs(positions[0].Y - positions[1].Y); 
+            /*
+            rect.X = (positions[0].X < positions[1].X) ? positions[0].X : positions[1].X;
+            rect.Y = (positions[0].Y < positions[1].Y) ? positions[0].Y : positions[1].Y; 
+            */
+
+            rect.Fill = brush;
+            rect.Stroke = brush;
+            
+
+            Canvas.SetLeft(rect, 50);
+            Canvas.SetTop(rect, 50);
+
+            
+        }
+    }
+}
